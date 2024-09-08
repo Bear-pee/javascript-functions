1a56be22e36ba9e54d044642faafcc522d3f72cb
Bear-pee <puringto5@gmail.com>
2024-09-08 16:29:31 +0700

    Update ex-5.js

--- ex-5.js
+++ ex-5.js
@@ -1,4 +1,4 @@
-function findMinNumber(a,b){return console.log(Math.min(a,b));};
+function findMinNumber(a,b){return Math.min(a,b);};
 const minNumber = findMinNumber(10, 20);
 
 console.log(minNumber); // 10
